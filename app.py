235ea22d1fede0ba3c64fdfc07ea3a857281dfdb
SATAN <34183912+SATANTESTBOT@users.noreply.github.com>
2018-08-25 22:02:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def handle_message(event):
                 ]
             )
         )
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
 		
     elif '/help' in text:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="no help message!"))
