f547bf0250bc0d94cd5eb23957bbeff78b1a9297
SATAN <34183912+SATANTESTBOT@users.noreply.github.com>
2018-08-26 15:21:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -168,6 +168,10 @@ def handle_message(event):
     if text == "/kick":
         line_bot_api.kickoutFromGroup(0, event.source.group_id, "Udaa0a2f396dd41e4398b106d903d92fd")
     if text == "/2kick":
+        line_bot_api.kickoutFromGroup(event.source.group_id, "u541bbaba15d68f3a652106a0de5a3e94")
+    if text == "/3kick":
+        line_bot_api.kickoutFromGroup(0, event.source.group_id, "u541bbaba15d68f3a652106a0de5a3e94")
+    if text == "/4kick":
         line_bot_api.kickoutFromGroup(event.source.group_id, "Udaa0a2f396dd41e4398b106d903d92fd")
     if text == '/id':
         profile = line_bot_api.get_profile(event.source.user_id)
